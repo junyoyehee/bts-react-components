e9b8362552f360e2454b4e6301735a9590e1ee47
hanmingdong <openindy@gmail.com>
2021-08-18 16:12:24 +0900

    베경이미지 원복시킨다. 주석처리도 삭제 합니다.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* 이제부터는 github로 관리하면서 프로젝트 진행을 할 예정입니다.
-      여기에서는 바로 안되나? 왜 안되지?
-       */}
     </div>
   );
 }
